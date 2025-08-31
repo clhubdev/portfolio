bd27c2fdaf1dbbc2cbc472ed39ed15de41874ea7
Clément HUBERT <clhubertmail@gmail.com>
2025-09-01 00:02:04 +0200

    add example work

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -45,6 +45,162 @@ export default function MyWork() {
                             </ul>
                         </div>
                     </div>
+
+                    <div className='projectCard'>
+                        <div className="cardContent">
+
+                            <span className='actions'>
+                                <IoLogoGithub style={{fontSize: "3rem", color:"#5a17eb"}}/>
+                                {/* <FaExternalLinkSquareAlt style={{fontSize: "3rem", color:"#5a17eb"}}/> */}
+                            </span>
+
+                            <h3>L'Épi du fermier </h3>
+
+                            <p className="hoverText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sit ratione suscipit! Ex corporis magni, ipsam cumque aperiam eligendi aliquam deleniti? Magnam quas dolore provident iusto, expedita odio recusandae voluptates.</p>
+
+                            <div></div>
+                        </div>
+
+                        <div className='tags'>
+                            <ul>
+                                <li>Symfony</li>
+                                <li>PHP</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>MySql</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='projectCard'>
+                        <div className="cardContent">
+
+                            <span className='actions'>
+                                <IoLogoGithub style={{fontSize: "3rem", color:"#5a17eb"}}/>
+                                {/* <FaExternalLinkSquareAlt style={{fontSize: "3rem", color:"#5a17eb"}}/> */}
+                            </span>
+
+                            <h3>L'Épi du fermier </h3>
+
+                            <p className="hoverText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sit ratione suscipit! Ex corporis magni, ipsam cumque aperiam eligendi aliquam deleniti? Magnam quas dolore provident iusto, expedita odio recusandae voluptates.</p>
+
+                            <div></div>
+                        </div>
+
+                        <div className='tags'>
+                            <ul>
+                                <li>Symfony</li>
+                                <li>PHP</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>MySql</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='projectCard'>
+                        <div className="cardContent">
+
+                            <span className='actions'>
+                                <IoLogoGithub style={{fontSize: "3rem", color:"#5a17eb"}}/>
+                                {/* <FaExternalLinkSquareAlt style={{fontSize: "3rem", color:"#5a17eb"}}/> */}
+                            </span>
+
+                            <h3>L'Épi du fermier </h3>
+
+                            <p className="hoverText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sit ratione suscipit! Ex corporis magni, ipsam cumque aperiam eligendi aliquam deleniti? Magnam quas dolore provident iusto, expedita odio recusandae voluptates.</p>
+
+                            <div></div>
+                        </div>
+
+                        <div className='tags'>
+                            <ul>
+                                <li>Symfony</li>
+                                <li>PHP</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>MySql</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='projectCard'>
+                        <div className="cardContent">
+
+                            <span className='actions'>
+                                <IoLogoGithub style={{fontSize: "3rem", color:"#5a17eb"}}/>
+                                {/* <FaExternalLinkSquareAlt style={{fontSize: "3rem", color:"#5a17eb"}}/> */}
+                            </span>
+
+                            <h3>L'Épi du fermier </h3>
+
+                            <p className="hoverText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sit ratione suscipit! Ex corporis magni, ipsam cumque aperiam eligendi aliquam deleniti? Magnam quas dolore provident iusto, expedita odio recusandae voluptates.</p>
+
+                            <div></div>
+                        </div>
+
+                        <div className='tags'>
+                            <ul>
+                                <li>Symfony</li>
+                                <li>PHP</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>MySql</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='projectCard'>
+                        <div className="cardContent">
+
+                            <span className='actions'>
+                                <IoLogoGithub style={{fontSize: "3rem", color:"#5a17eb"}}/>
+                                {/* <FaExternalLinkSquareAlt style={{fontSize: "3rem", color:"#5a17eb"}}/> */}
+                            </span>
+
+                            <h3>L'Épi du fermier </h3>
+
+                            <p className="hoverText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sit ratione suscipit! Ex corporis magni, ipsam cumque aperiam eligendi aliquam deleniti? Magnam quas dolore provident iusto, expedita odio recusandae voluptates.</p>
+
+                            <div></div>
+                        </div>
+
+                        <div className='tags'>
+                            <ul>
+                                <li>Symfony</li>
+                                <li>PHP</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>MySql</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='projectCard'>
+                        <div className="cardContent">
+
+                            <span className='actions'>
+                                <IoLogoGithub style={{fontSize: "3rem", color:"#5a17eb"}}/>
+                                {/* <FaExternalLinkSquareAlt style={{fontSize: "3rem", color:"#5a17eb"}}/> */}
+                            </span>
+
+                            <h3>L'Épi du fermier </h3>
+
+                            <p className="hoverText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo sit ratione suscipit! Ex corporis magni, ipsam cumque aperiam eligendi aliquam deleniti? Magnam quas dolore provident iusto, expedita odio recusandae voluptates.</p>
+
+                            <div></div>
+                        </div>
+
+                        <div className='tags'>
+                            <ul>
+                                <li>Symfony</li>
+                                <li>PHP</li>
+                                <li>CSS</li>
+                                <li>Javascript</li>
+                                <li>MySql</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
 
