50c079765d9668618d70ae4d1f28008b7e8563cf
Clément HUBERT <clhubertmail@gmail.com>
2025-08-31 23:57:14 +0200

    add link

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,6 +5,7 @@ import { BsTelephoneOutbound } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaRegMap } from "react-icons/fa";
+import Link from "next/link";
 
 
 export default function Contact() {
@@ -21,49 +22,65 @@ export default function Contact() {
 
                     <div className={styles.infosList}>
                         <div className={styles.infosItem}>
-                            <FiMail style={{fontSize: "2rem"}}/>
+                            <FiMail style={{ fontSize: "2rem" }} />
                             <div>
-                                <div style={{marginBottom: "0.5rem"}}>Email</div>
+                                <div style={{ marginBottom: "0.5rem" }}>Email</div>
                                 <div>clhubertmail@gmail.com</div>
                             </div>
                         </div>
 
                         <div className={styles.infosItem}>
-                            <BsTelephoneOutbound style={{fontSize: "2rem"}}/>
+                            <BsTelephoneOutbound style={{ fontSize: "2rem" }} />
                             <div>
-                                <div style={{marginBottom: "0.5rem"}}>Téléphone</div>
+                                <div style={{ marginBottom: "0.5rem" }}>Téléphone</div>
                                 <div>06.23.55.07.18</div>
                             </div>
                         </div>
 
                         <div className={styles.infosItem}>
-                            <FaGithub style={{fontSize: "2rem"}}/>
+                            <FaGithub style={{ fontSize: "2rem" }} />
                             <div>
-                                <div style={{marginBottom: "0.5rem"}}>Github</div>
+                                <div style={{ marginBottom: "0.5rem" }}>Github</div>
                                 <div>clhubdev</div>
                             </div>
                         </div>
 
                         <div className={styles.infosItem}>
-                            <FaLinkedin style={{fontSize: "2rem"}}/>
+                            <FaLinkedin style={{ fontSize: "2rem" }} />
                             <div>
-                                <div style={{marginBottom: "0.5rem"}}>LinkedIn</div>
+                                <div style={{ marginBottom: "0.5rem" }}>LinkedIn</div>
                                 <div>in/clementh-webdev</div>
                             </div>
                         </div>
 
                         <div className={styles.infosItem}>
-                            <FaRegMap style={{fontSize: "2rem"}}/>
+                            <FaRegMap style={{ fontSize: "2rem" }} />
                             <div>
-                                <div style={{marginBottom: "0.5rem"}}>Mobilité</div>
+                                <div style={{ marginBottom: "0.5rem" }}>Mobilité</div>
                                 <div>Ile-de-France</div>
                             </div>
                         </div>
                     </div>
 
                     <div className={styles.actions}>
-                        <button>Télécharger mon CV</button>
-                        <button>Voir mes projets</button>
+                        <Link
+                            href='/CV_HUBERT_Clément_dev_web_FS.pdf'
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="CV HUBERT Clément au format PDF"
+                            title="Voir mon CV"
+                        >
+                            <button>Télécharger mon CV</button>
+                        </Link>
+
+                        <Link
+                            href='/#myWork'
+                            rel="noopener noreferrer"
+                            aria-label="Lien vers section mes projets"
+                            title="Voir mes projets"
+                        >
+                            <button>Voir mes projets</button>
+                        </Link>
                     </div>
                 </section>
 
@@ -73,7 +90,7 @@ export default function Contact() {
                         <p>Promis je vérifie ma boîte mail régulièrement, même les spams</p>
                     </div>
 
-                    <div style={{height: "100%"}}>
+                    <div style={{ height: "100%" }}>
                         <ContactForm />
                     </div>
                 </section>
