c015cf1da656c5d18b33a9444f0828045ba8237a
Clément HUBERT <clhubertmail@gmail.com>
2025-09-02 18:44:30 +0200

    delete import Script useless

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
